57c8eb080ee1a86069c8613cc3835d796c4487eb
George Lukas Wagner <gwagner@c2r13s4.42wolfsburg.de>
2024-08-23 15:16:59 +0200

    added sigint  and sigquit (need to fix sigquit later)

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: gwagner <gwagner@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/18 13:35:12 by gwagner           #+#    #+#             */
-/*   Updated: 2024/08/23 13:20:55 by gwagner          ###   ########.fr       */
+/*   Updated: 2024/08/23 15:07:15 by gwagner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,13 +31,37 @@ void	lain_loop(char **envp)
 			args = split_args(lain);
 			syntax_error(args);
 			free_list(&args);
+			free(lain);
 		}
 	}
+}
+
+void	signal_handler(int signum)
+{
+	g_signum = signum;
+	if (signum == SIGINT)
+	{
+		ft_putstr_fd("\n", 1);
+		rl_on_new_line();
+		rl_redisplay();
+	}
+	else if (signum == SIGQUIT)
+	{
+		rl_on_new_line();
+		rl_redisplay();
+	}
+}
+
+void	init_signal(void)
+{
+	signal(SIGINT, signal_handler);
+	signal(SIGQUIT, signal_handler);
 }
 
 int	main(int ac, char **av, char **envp)
 {
 	(void)ac;
 	(void)av;
+	init_signal();
 	lain_loop(envp);
 }
